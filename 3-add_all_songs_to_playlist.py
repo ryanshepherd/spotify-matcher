caba8de312ce595f7693c7de73d42cdaf81cf75f
Ryan Shepherd <rjshep@gmail.com>
2024-06-16 18:34:15 -0400

    Script to add all songs to a playlist

---
+++ 3-add_all_songs_to_playlist.py
@@ -0,0 +1,148 @@
+"""
+Goal: Add all songs from the albums that I like to a "My CDs" playlist. This way
+I can shuffle my entire song collection.
+"""
+
+#%%
+import os
+import time
+from typing import Dict, List
+import spotipy
+import spotipy.util as util
+import pandas as pd
+
+username = os.environ["SPOTIFY_USERNAME"]
+scope = "user-library-read, user-library-modify, user-follow-modify, user-follow-read, playlist-read-private,  playlist-modify-public,  playlist-modify-private"
+
+# Get an auth token
+token = util.prompt_for_user_token(username, scope)
+if token:
+    sp = spotipy.Spotify(auth=token)
+else:
+    print("Can't get token for", username)
+
+
+
+#%%
+##############################
+# Define some functions
+##############################
+
+# Get all followed artists
+def get_all_followed_artists() -> List[Dict]:
+    last_id = None
+    CHUNK_SIZE = 50
+    existing_follows = []
+    while True:
+        resp = sp.current_user_followed_artists(limit=CHUNK_SIZE, after=last_id)
+        artists = ([{"id": item["id"], "name": item["name"]} for item in resp["artists"]["items"]])
+        print(f"Retrieved {len(artists)} artists. Sample: {', '.join([item['name'] for item in artists[0:5]])}")
+
+        existing_follows += artists
+
+        if len(resp["artists"]["items"]) < CHUNK_SIZE:
+            break
+
+        last_id = artists[-1]["id"]
+        
+        time.sleep(.5)
+
+    return existing_follows
+
+def get_all_saved_albums() -> List[Dict]:
+    offset = 0
+    CHUNK_SIZE = 50
+    existing_follows = []
+    while True:
+        resp = sp.current_user_saved_albums(limit=CHUNK_SIZE, offset=offset)
+        albums = ([{
+            "id": item["album"]["id"],
+            "name": item["album"]["name"],
+            "artist_id": item["album"]["artists"][0]["id"],
+            "artist_name": item["album"]["artists"][0]["name"],
+        } for item in resp["items"]])
+        print(f"Retrieved {len(albums)} albums. Sample: {', '.join([item['name'] for item in albums[0:5]])}")
+
+        existing_follows += albums
+
+        if len(resp["items"]) < CHUNK_SIZE:
+            break
+
+        offset += CHUNK_SIZE
+        
+        time.sleep(.5)
+
+    return existing_follows
+
+
+def get_all_albums_for_artists(artists: List[str]):
+    artist_albums = []
+    for i, artist_id in enumerate(artists):
+        print(f"Retrieving albums for artists {i} / {len(artists)}: {artist_id}...", end="")
+        
+        resp = sp.artist_albums(artist_id, album_type="album",limit=50)    
+
+        if len(resp["items"]) > 0:
+            albums = [{
+                    "artist_id": artist_id,
+                    "album_id": item["id"],
+                    "name": item["name"],
+                    "url": item["external_urls"]["spotify"]
+                } for item in resp["items"]]
+
+            print(f"{len(albums)} records. Samples: {', '.join([item['name'] for item in albums[0:3]])}")
+
+            artist_albums += albums
+        else:
+            print("None found.")
+
+        time.sleep(.5)
+
+    return pd.DataFrame(artist_albums)
+
+def chunker(seq, size):
+    return (seq[pos:pos + size] for pos in range(0, len(seq), size))
+
+#%%
+
+# Get list of albums that I like
+albums = get_all_saved_albums()
+
+#%%
+# For each album, add all songs on the album to the "My CDs" playlist
+
+PLAYLIST_NAME = "My CDs"
+
+# Get the playlist ID
+playlists = sp.current_user_playlists()
+playlist = [playlist for playlist in playlists["items"] if playlist["name"] == PLAYLIST_NAME][0]
+
+#%%
+# Get all track IDs across all albums
+track_ids = []
+for album in albums:
+    print(f"Retrieving tracks for {album['name']} by {album['artist_name']}...")
+    resp = sp.album_tracks(album["id"])
+    track_ids += [item["id"] for item in resp["items"]]
+
+#%%
+
+len(track_ids)
+
+#%%
+
+# If more than 10k tracks, cut off at 10k
+if len(track_ids) > 10000:
+    print(f"Warning: More than 10k tracks found ({len(track_ids)}). Truncating to 10k.")
+    track_ids = track_ids[:10000]
+
+#%%
+# Empty out the playlist
+sp.user_playlist_replace_tracks(username, playlist["id"], [])
+
+#%%
+# Add 100 tracks at a time
+for chunk in chunker(track_ids, 100):
+    sp.user_playlist_add_tracks(username, playlist["id"], chunk)
+
+print(f"Added {len(track_ids)} tracks from {album['name']} by {album['artist_name']} to {PLAYLIST_NAME}.")
